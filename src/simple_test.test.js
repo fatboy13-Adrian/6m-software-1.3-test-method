3a0ad33a72e0590c3b4fd6536674ee7ab43dd2bf
fatboy13-Adrian <birdbird_dog@hotmail.sg>
2024-11-28 15:39:22 +0800

    Updated simple test.test.js file
    Updated simple test.test.js file.
    Checked and confirmed that all tests / test suites had changed to passed.

--- src/simple_test.test.js
+++ src/simple_test.test.js
@@ -5,7 +5,7 @@ describe("Test Suite for sample.js functions", () =>
     it("should add 3 numbers", () =>
     {
         const result = addFunc(1, 3, 4);
-        expect(result).toBe(7);
+        expect(result).toBe(8);
     });//End of it should add 3 numbers
 
     it("should check divisibility by 5", () => 
